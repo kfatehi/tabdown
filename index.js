1479aff3f3c4b5ae86d57b711d923ec223d15e39
Keyvan Fatehi <keyvanfatehi@gmail.com>
2016-08-18 18:38:07 -0700

    usable index.js; commonjs require; node replacer
    create index.js entrypoint with a parse function
    use CommonJS require instead of ES6 import (for electron)
    add a replaceNode option to customize the nodes before add

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// TODO: implement
+var Parser = require('./lib/parser');
+
+module.exports = {
+  parse: function(str, opts) {
+    var parser = new Parser(opts);
+    return parser.parse(str);
+  }
+}
